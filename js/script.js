c70d5d6b8bc5da298400167c34165a9065c894ec
GustavoPetrentchuk <gustavo1petrentchuk@gmail.com>
2022-06-27 17:13:34 -0300

    Adicionado demais páginas

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ const btn = document.querySelector(".btn-mode");
 var tema = document.getElementById('tema');
 
 function mudarTema(){
-    if (tema.getAttribute('href') == 'css/styles.css'){
-        tema.setAttribute('href', 'css/styles-dark.css');
+    if (tema.getAttribute('href') == '../css/styles.css'){
+        tema.setAttribute('href', '../css/styles-dark.css');
+        document.cookie = "css=Dark";
     } else {
-        tema.setAttribute('href', 'css/styles.css');
+        tema.setAttribute('href', '../css/styles.css');
+        document.cookie = "css=Light";
     }
 }
 
